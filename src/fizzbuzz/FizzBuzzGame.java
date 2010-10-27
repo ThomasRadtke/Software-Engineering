5f8bf91a46db853fb40aa6aa0bbf5777eab8f8f0
Admin <thomas.radtke@ewetel.net>
2010-10-27 14:57:47 +0200

    Game with PopUp

--- src/fizzbuzz/FizzBuzzGame.java
+++ src/fizzbuzz/FizzBuzzGame.java
@@ -10,37 +10,74 @@ public class FizzBuzzGame {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("############################");
-		System.out.println("###     FizzBuzzGame     ###");
-		System.out.println("############################");
-		System.out.println();
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("Bitte Anzahl der Level wählen (1 bis ~): ");
-		Input = scanner.next();
+		//System.out.println("############################");
+		//System.out.println("###     FizzBuzzGame     ###");
+		//System.out.println("############################");
+		//System.out.println();
+		//Scanner scanner = new Scanner(System.in);
+		//System.out.print("Bitte Anzahl der Level wählen (1 bis ~): ");
+		//Input = scanner.next();
 		
-		if (!Input.matches("[0-9]+") || Integer.parseInt(Input) < 1 ) {
-			do {
-				System.out.print("Bitte Eingabe korrigieren: ");
-				Input = scanner.next();
-					
-			} while (!Input.matches("[0-9]+") || Integer.parseInt(Input) < 1); 
+		String [] Level = {"Leicht (25)", "Normal (100)", "Schwer (250)"};
+		int Choice = JOptionPane.showOptionDialog(null
+													, "Bitte Schwierigkeitsgrad wählen"
+													, "FizzBuzzGame"
+													, JOptionPane.OK_CANCEL_OPTION 
+													, JOptionPane.PLAIN_MESSAGE 
+													, null
+													, Level 
+													, Level[0]);
+		
+		if (Choice == -1) {
+			JOptionPane.showMessageDialog(null,"Vielen Dank fürs Spielen!","FizzBuzzGame",JOptionPane.INFORMATION_MESSAGE);
+			System.exit(0);
 		}
 		
-		n = Integer.parseInt(Input); 
-		System.out.println();
-		System.out.println("###      Los geht's!      #");
-		System.out.println();
+		if (Choice == 1) {
+			n = 25;
+		}
+		if (Choice == 2) {
+			n = 100;
+		}
+		if (Choice == 3) {
+			n = 250;
+		}
 		
+		//if (!Input.matches("[0-9]+") || Integer.parseInt(Input) < 1 ) {
+		//	do {
+		//		System.out.print("Bitte Eingabe korrigieren: ");
+		//		Input = scanner.next();
+					
+		//	} while (!Input.matches("[0-9]+") || Integer.parseInt(Input) < 1); 
+		//}
+		
+		// n = Integer.parseInt(Input); 
+		
+		JOptionPane.showMessageDialog(null,"Los geht's!","FizzBuzzGame",JOptionPane.INFORMATION_MESSAGE);
+				
 		for (int i = 1; i <= n; i++) {
 			
-			//String UserInput = JOptionPane.showInputDialog(null, i+" - Fizz - Buzz - FizzBuzz", "Bitte ihre Lösung auswählen", JOptionPane.QUESTION_MESSAGE);
-			String UserInput = JOptionPane.showInputDialog(null, "Next?", "Bitte ihre Lösung auswählen", JOptionPane.QUESTION_MESSAGE);
-			if (UserInput.equals(Result(i))) {
-				JOptionPane.showMessageDialog(null,"Richtig!","Die Antwort war korrekt",JOptionPane.INFORMATION_MESSAGE);
+			String LevelCode = String.valueOf(i); 
+			String [] Choices = {LevelCode, "Fizz", "Buzz", "FizzBuzz"};
+			int Response = JOptionPane.showOptionDialog(null
+														, "Next?"
+														, "Bitte auswählen"
+														, JOptionPane.OK_CANCEL_OPTION 
+														, JOptionPane.PLAIN_MESSAGE 
+														, null
+														, Choices 
+														, Choices[0]);
+			
+			if (Response == -1) {
+				JOptionPane.showMessageDialog(null,"Vielen Dank fürs Spielen!","FizzBuzzGame",JOptionPane.INFORMATION_MESSAGE);
+				System.exit(0);
 			}
-			else {
-				JOptionPane.showMessageDialog(null,"Level #"+i,"Leider verloren!",JOptionPane.INFORMATION_MESSAGE);
-				JOptionPane.showMessageDialog(null,"Level #"+i,"Korrekte Antwort lautete:"+Result(i),JOptionPane.INFORMATION_MESSAGE);
+			
+			String UserInput = Choices[Response];
+			
+			if (!UserInput.equals(Result(i))) {
+				JOptionPane.showMessageDialog(null,"Leider verloren!","Level #"+i,JOptionPane.INFORMATION_MESSAGE);
+				JOptionPane.showMessageDialog(null,"Korrekte Antwort lautete: "+Result(i),"Level #"+i,JOptionPane.INFORMATION_MESSAGE);
 				System.exit(0);
 			}
 		}
